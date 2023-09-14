024921e07e0ef701dedadb98357ab185c3064c85
kekimate <115459634+kekimate@users.noreply.github.com>
2023-09-14 21:10:06 +0200

    Delete java-main directory

--- java-main/FavoriteNumber.java
+++
@@ -1,9 +0,0 @@
-
-public class FavoriteNumber {
-    public static void main(String[] args) {
-
-        int number = 13;
-        
-        System.out.println("My favorite number is: " + number);
-    }
-}
--- java-main/PrintEven.java
+++
@@ -1,8 +0,0 @@
-public class PrintEven {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 500; i += 2)
-        {
-            System.out.println(i);
-        }
-    }
-}

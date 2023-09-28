266cd57edc70b4872c9dc4ecf28a68e12d0c20d1
kekimate <115459634+kekimate@users.noreply.github.com>
2023-09-28 14:56:40 +0200

    Add files via upload

---
+++ java-main-KM/Fibonacci.java
@@ -0,0 +1,50 @@
+public class Fibonacci {
+
+    //Ciklussal
+    public static int fibonacci(int n)
+    {
+        if (n < 0)
+        {
+            System.out.println("Az n nem lehet negatív.");
+        }
+
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        int elozo = 0;
+        int jelen = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            int next = elozo + jelen;
+            elozo = jelen;
+            jelen = next;
+        }
+        return jelen;
+    }
+
+    //Rekurzívan
+    public static int fibonacciRec(int n)
+    {
+        if (n < 0)
+        {
+            System.out.println("Az n nem lehet negatív.");
+        }
+
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        return fibonacciRec(n - 1) + fibonacciRec(n - 2);
+    }
+
+    public static void main(String[] args)
+    {
+        int n = 10;
+        System.out.println("Fibonacci(" + n + ") ciklussal: " + fibonacci(n));
+        System.out.println("FibonacciRec(" + n + ") rekurzívan: " + fibonacciRec(n));
+    }
+}

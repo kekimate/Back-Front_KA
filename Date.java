8cbfcc5ad1f5ff399483a2aad3e48f4f3d307db4
kekimate <115459634+kekimate@users.noreply.github.com>
2023-10-10 20:22:48 +0200

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,66 @@
+public class Date {
+    private int year;
+    private int month;
+    private int day;
+
+    public Date(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public boolean isCorrectDate() {
+        if (month < 1 || month > 12)
+            return false;
+        if (day < 1)
+            return false;
+        if (month == 2) {
+            if (isSzoko()) {
+                return day <= 29;
+            } else {
+                return day <= 28;
+            }
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return day <= 30;
+        } else {
+            return day <= 31;
+        }
+    }
+
+    public void printDate() {
+        System.out.println("Dátum: " + year + "." + String.format("%02d", month) + "." + String.format("%02d", day));
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    private boolean isSzoko() {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        Date date = new Date(2023, 5, 30);
+        date.printDate();
+    }
+}

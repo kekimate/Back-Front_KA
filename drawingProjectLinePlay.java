8d69e6a5822ab2f9bd7883c4d91a164e5d4117ca
kekimate <115459634+kekimate@users.noreply.github.com>
2023-10-10 11:45:43 +0200

    Add files via upload

---
+++ drawingProjectLinePlay.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class drawingProjectLinePlay {
+    public static void drawImage(Graphics graphics) {
+        int size = WIDTH / 2;
+        /*
+        graphics.setColor(new Color(0, 49, 63));
+        graphics.fillRect(0, 0, WIDTH, HEIGHT);
+        graphics.setColor(Color.yellow);
+        */
+        drawSomething(0, 0, size, graphics);
+    }
+
+    public static void drawSomething(int x, int y ,int size, Graphics graphics) {
+
+        //ezt kell átírni
+		int a = 20;
+		int b = 15;
+		for(int i = 0; i < 12; i++)
+		{
+			graphics.drawLine(0, a, b, 250);
+			graphics.drawLine(250, b, a, 0);
+			a = a + 20;
+			b = b + 15;
+		}
+    }
+
+    // ezt a részt nem kell módosítani
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            drawImage(graphics);
+        }
+    }
+}
